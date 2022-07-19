740a84799c42b4f460cac3a22f543b8884c45c6e
VietThanLinux <thanhoangviet@gmail.com>
2022-07-18 20:40:58 -0400

    more view classes

--- more_view_classes/tutorial/tests.py
+++ more_view_classes/tutorial/tests.py
@@ -1,63 +1,46 @@
 # import unittest
-# 
+
 # from pyramid import testing
-# 
-# 
+
+
 # class TutorialViewTests(unittest.TestCase):
 #     def setUp(self):
 #         self.config = testing.setUp()
-# 
+
 #     def tearDown(self):
 #         testing.tearDown()
-# 
+
 #     def test_home(self):
-#         from .views import home
-# 
+#         from .views import TutorialViews
+
 #         request = testing.DummyRequest()
-#         response = home(request)
-#         # Our view now returns data
-#         self.assertEqual('Home View', response['name'])
-# 
-#     def test_hello(self):
-#         from .views import hello
-# 
-#         request = testing.DummyRequest()
-#         response = hello(request)
-#         # Our view now returns data
-#         self.assertEqual('Hello View', response['name'])
-# 
-# 
+#         inst = TutorialViews(request)
+#         response = inst.home()
+#         self.assertEqual('Home View', response['page_title'])
+
 # class TutorialFunctionalTests(unittest.TestCase):
 #     def setUp(self):
 #         from tutorial import main
 #         app = main({})
 #         from webtest import TestApp
-# 
+
 #         self.testapp = TestApp(app)
-# 
+
 #     def test_home(self):
 #         res = self.testapp.get('/', status=200)
-#         self.assertIn(b'<h1>Hi Home View', res.body)
-# 
-#     def test_hello(self):
-#         res = self.testapp.get('/howdy', status=200)
-#         self.assertIn(b'<h1>Hi Hello View', res.body)
+#         self.assertIn(b'TutorialViews - Home View', res.body)
 
 from pyramid import testing
 
-def test_home():
-    from .views import home
+class TestTutorial:
 
-    request = testing.DummyRequest()
-    response = home(request)
-    assert 'Home View' == response['name']
+    def test_home(self):
+        from .views import TutorialViews
 
-def test_hello():
-    from .views import hello
-
-    request = testing.DummyRequest()
-    response = hello(request)
-    assert 'Hello View' == response['name']
+        request = testing.DummyRequest()
+        inst = TutorialViews(request)
+        response = inst.home()
+        assert 'Home View' == response['page_title']
 
 
 class TestFunctional():
@@ -68,8 +51,4 @@ class TestFunctional():
 
     def test_home_functional(self):
         res = self.testapp.get('/', status=200)
-        assert b'<h1>Hi Home View' in res.body
-
-    def test_hello_functional(self):
-        res = self.testapp.get('/howdy', status=200)
-        assert b'<h1>Hi Hello View' in res.body
+        assert b'TutorialViews - Home View' in res.body
