04ffc34ef3e6310309caf6c58f60d37abe82e8f2
VietThanLinux <thanhoangviet@gmail.com>
2022-07-19 21:34:11 -0400

    logging

--- sessions/tutorial/views.py
+++ sessions/tutorial/views.py
@@ -1,11 +1,7 @@
-import logging
-log = logging.getLogger(__name__)
-
 from pyramid.view import (
     view_config,
     view_defaults
-    )
-
+)
 
 @view_defaults(renderer='home.pt')
 class TutorialViews:
@@ -14,10 +10,8 @@ class TutorialViews:
 
     @view_config(route_name='home')
     def home(self):
-        log.debug('In home view')
         return {'name': 'Home View'}
 
     @view_config(route_name='hello')
     def hello(self):
-        log.debug('In hello view')
         return {'name': 'Hello View'}
